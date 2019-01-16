a906acd8dcb3551398ce61236e6982067a890644
muhammad-ammar <mammar@gmail.com>
2019-01-16 17:05:43 +0500

    feat(logging): export logging util

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { fetchUserProfile } from './actions/userProfile';
 import getAuthenticatedAPIClient from './AuthenticatedAPIClient';
 import PrivateRoute from './PrivateRoute';
 import userProfile from './reducers/userProfile';
+import * as logging from './logging';
 
 export {
   fetchUserProfile,
   getAuthenticatedAPIClient,
   PrivateRoute,
   userProfile,
+  logging,
 };
